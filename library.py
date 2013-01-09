edab6b47e2947f919b9651b3de10477d010982d2
SiroDiazPalazon <diazpalazonsiro@gmail.com>
2013-01-09 10:38:40 +0100

    Update library.py

--- library.py
+++ library.py
@@ -1,2 +1,14 @@
 # Library developed to realize operations over a matrix
 # now in development...
+
+
+class Matrix:
+        
+        def __init__(self, mtxRows):
+                self.mtxRows = mtxRows;
+                if len(self.mtxRows) == 0 and type(self.mtxRows) != 'list':
+                        # show a error message...
+                elif #...
+                
+                else:
+                        # continue...
