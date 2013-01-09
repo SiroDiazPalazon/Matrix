26ff9226d25924f503b76839fee31cad7bc75254
SiroDiazPalazon <diazpalazonsiro@gmail.com>
2013-01-09 23:50:22 +0100

    Update library.py

--- library.py
+++ library.py
@@ -1,14 +1,16 @@
 # Library developed to realize operations over a matrix
-# now in development...
+# now under development...
 
+import threading, math, time, sys 
 
 class Matrix:
         
         def __init__(self, mtxRows):
                 self.mtxRows = mtxRows;
-                if len(self.mtxRows) == 0 and type(self.mtxRows) != 'list':
-                        # show a error message...
-                elif #...
-                
+                if len(self.mtxRows) >= 0 and type(self.mtxRows) == 'list':
+                        # initialize the matrix
                 else:
-                        # continue...
+                        # return a error message
+                        # studying the case
+                        
+                
