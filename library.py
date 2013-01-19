deae45be2fb2cdaf09b9044cf8a3b822653040a4
SiroDiazPalazon <diazpalazonsiro@gmail.com>
2013-01-19 01:31:27 +0100

    new methods added

--- library.py
+++ library.py
@@ -6,11 +6,34 @@ import threading, math, time, sys
 class Matrix:
         
         def __init__(self, mtxRows):
-                self.mtxRows = mtxRows;
+                self.mtxRows = mtxRows
+                self.lenMatrix = len(self.mtxRows)
+                self.numElemRows = []
                 if len(self.mtxRows) >= 0 and type(self.mtxRows) == 'list':
                         # initialize the matrix
                 else:
                         # return a error message
                         # studying the case
-                        
+        
+        def _isSquare(self):
+                index = 0
+                control = False
+                for i in self.mtxRows:
+                        for j in a[index]:
+                                # insert into the subarrays length
+                                self.numElemRows = [len(x) for x in self.mtxRows if len(x) == len(self.mtxRows)]
+                        index += 1
+                for k in self.numElemRows:
+                        # will continue...
+                           
+                           
+        def _rangeMatrix(self):
                 
+        
+        def _printMatrix(self):
+                
+                        
+        def _determinantMatrix(self):
+                
+                for i in self.mtxRows:
+                        # traverse the first array layer
